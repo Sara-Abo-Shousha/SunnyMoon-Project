c3dd9d7d53088661e3461e6e112618b3b84a71b2
Sara-Abo-Shousha <77673106+Sara-Abo-Shousha@users.noreply.github.com>
2022-11-22 22:09:01 +0200

    Update main.js

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,10 +1,4 @@
 
-window.localStorage.setItem('page', 'index.html');
-var startPage = window.localStorage.getItem('page');
-
-if (startPage) {
-    window.location.replace(startPage);
-}
 var swiper = new Swiper(".mySwiper", {
   effect: "coverflow",
   grabCursor: true,
